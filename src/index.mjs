b30f18d4e6c1f265418f718b4073922292559f8b
Poroca <gustavoporoca@gmail.com>
2024-07-04 08:40:43 -0300

    adding recomended extensios

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,17 @@
-import agendamentoRoutes from "./routes/agendamento.router.mjs";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import express from "express";
+import agendamentoRoutes from "./routes/agendamento.router.mjs";
+
 
 const server = express();
 
 const PORT = process.env.PORT || 3000;
 
+server.use(cors());
+server.use(helmet());
+server.use(morgan("combined")); 
 server.use(agendamentoRoutes);
 
 server.use("*", (request, response) => {
